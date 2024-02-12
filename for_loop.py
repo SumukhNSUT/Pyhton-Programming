037cbe422f5f28adecd253aa7d371fbe1e8bc8e4
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-02-12 19:19:31 +0530

    for loop

---
+++ for_loop.py
@@ -0,0 +1,7 @@
+# Define a list of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# Iterate over each number in the list using a for loop
+for num in numbers:
+    # Print each number multiplied by 2
+    print(num * 2)
