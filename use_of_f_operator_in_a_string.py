1b7caad02ec63dfdba40e6d81318eb2cba32cb95
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-02-13 16:56:36 +0530

    f operator

---
+++ use_of_f_operator_in_a_string.py
@@ -0,0 +1,8 @@
+name = "Sumukh"
+age = 18
+print(f"My name is {name} and I am {age} years old.")
+print("My name is "+str(name) + " and I am "+str(age) + " years old.")
+# first method is a better way
+# as it is more readable
+# f stands for string formatter
+# it is operator for formatting strings to make more readable
