1df14153d14eba46ccc63ffe166f1113a4dad9ac
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-04-21 18:46:59 +0530

    commit

--- matplotlib.py
+++ matplotlib.py
@@ -1,12 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-x = np.arange(1, 11)
-y = 2*x + 5
+x = np.arange(0, 10)
+y = x + 1
 
 plt.plot(x, y)
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Plot of y = 2x + 5')
-plt.grid(True)
 plt.show()
