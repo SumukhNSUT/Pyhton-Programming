08666b336885e27b0f67455d9e8998cd2c66eb7c
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-04-21 20:14:19 +0530

    matplotlib basics

--- my_matplotlib.py
+++ my_matplotlib.py
@@ -1,8 +1,23 @@
+# import numpy as np
+# from matplotlib import pyplot as plt
+
+# x = np.arange(0, 5)
+# y = x
+
+# plt.plot(x, y)
+# plt.show()
+
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 
-x = np.arange(0, 10)
-y = x + 1
+x = np.arange(1, 11)
+y1 = x + 1
+y2 = x + 5
+plt.title("Matplotlib demo")
+plt.xlabel("x axis caption")
+plt.ylabel("y axis caption")
+plt.plot(x, y1, label="student 1")
+plt.plot(x, y2, label="student 2")
+plt.legend(loc=2)
 
-plt.plot(x, y)
 plt.show()
